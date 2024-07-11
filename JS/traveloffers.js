3cbc5ba6d10d6a45eb368560c28af9cf7ac0a504
flagz_ <khoint828@proton.me>
2024-07-11 18:19:28 +0300

    update 2 on traveloffers

--- JS/traveloffers.js
+++ JS/traveloffers.js
@@ -1,4 +1,8 @@
+const hto_btn_left = document.querySelector(".hto-btn-left");
+const hto_btn_right = document.querySelector(".hto-btn-right");
+const hottest_travel_offers = document.querySelector(".hottest-travel-offers");
 
+let HTOslideIndex = 0;
 
 function checkSignedinOrNot(){
     var LSSignedInStatus = localStorage.getItem("SignedInStatus");
@@ -9,3 +13,18 @@ function checkSignedinOrNot(){
       signedout.classList.toggle("hidden");
     } else {};
   }
+
+function HTOmoveSlides(){
+  hottest_travel_offers.style.transform = `translateX(-${HTOslideIndex * 100}vw)`;
+}
+
+function HTOcheckSlides(){
+  const HTOslidesArray = [...hottest_travel_offers.querySelectorAll(".hto-card")];
+  if(HTOslideIndex === HTOslidesArray.length){
+    hottest_travel_offers.style.transition = 'none';
+    HTOslideIndex = 0;
+    HTOmoveSlides()
+  }
+}
+
+hto_btn_left.addEventListener
