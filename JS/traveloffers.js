09771f34026f81f0bd7e04fe0fc64e0ffb9fc606
flagz_ <khoint828@proton.me>
2024-07-14 19:28:41 +0300

    traveloffers update 3

--- JS/traveloffers.js
+++ JS/traveloffers.js
@@ -1,30 +1,51 @@
+const hottest_travel_offers = document.querySelector(".hottest-travel-offers");
+const hto_items = document.querySelectorAll(".hto-card");
+const hto_dots = document.querySelectorAll(".hto-dots li");
 const hto_btn_left = document.querySelector(".hto-btn-left");
 const hto_btn_right = document.querySelector(".hto-btn-right");
-const hottest_travel_offers = document.querySelector(".hottest-travel-offers");
 
-let HTOslideIndex = 0;
+let HTOactive = 0;
+let lengthHTOItems = hto_items.length;
 
-function checkSignedinOrNot(){
-    var LSSignedInStatus = localStorage.getItem("SignedInStatus");
-    var SSSignedInStatus = sessionStorage.getItem("SignedInStatus");
-  
-    if (LSSignedInStatus === "true" || SSSignedInStatus === "true") {
-      signedin.classList.toggle("hidden");
-      signedout.classList.toggle("hidden");
-    } else {};
+function debug(){console.log(hto_items);}
+
+hto_btn_right.onclick = function () {
+  if (HTOactive + 1 >= lengthHTOItems) {
+    HTOactive = 0;
+  } else {
+    HTOactive = HTOactive + 1;
   }
+  reloadHTOSlider();
+};
 
-function HTOmoveSlides(){
-  hottest_travel_offers.style.transform = `translateX(-${HTOslideIndex * 100}vw)`;
+hto_btn_left.onclick = function () {
+  if (HTOactive - 1 < 0) {
+    HTOactive = lengthHTOItems - 1;
+  } else {
+    HTOactive = HTOactive - 1;
+  }
+  reloadHTOSlider();
+};
+
+let refreshHTOSlider = setInterval(() => {
+  hto_btn_right.click();
+}, 3000);
+
+function reloadHTOSlider() {
+  hottest_travel_offers.style.transition = `transform 1s cubic-bezier(0.83, 0, 0.17, 1)`
+  hottest_travel_offers.style.transform = `translateX(-${HTOactive * 100}vw)`;
+
+  const lastActiveHTODot = document.querySelector(".hto-dots li.active");
+  lastActiveHTODot.classList.remove("active");
+  hto_dots[HTOactive].classList.add("active");
+
+  clearInterval(refreshHTOSlider);
 }
 
-function HTOcheckSlides(){
-  const HTOslidesArray = [...hottest_travel_offers.querySelectorAll(".hto-card")];
-  if(HTOslideIndex === HTOslidesArray.length){
-    hottest_travel_offers.style.transition = 'none';
-    HTOslideIndex = 0;
-    HTOmoveSlides()
-  }
-}
+hto_dots.forEach((li, key) => {
+  li.addEventListener("click", function () {
+    HTOactive = key;
+    reloadHTOSlider();
+  });
+});
 
-hto_btn_left.addEventListener
